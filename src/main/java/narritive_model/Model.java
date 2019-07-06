7db365370a39ffbb0a53a8f4e5d3f3b88daac370
Alex Morgan <alexrmorgan@outlook.com>
2019-07-06 12:47:59 +1200

    Alter model to allow retrieval of values

--- src/main/java/narritive_model/Model.java
+++ src/main/java/narritive_model/Model.java
@@ -2,27 +2,39 @@ package narritive_model;
 
 import edu.stanford.nlp.pipeline.CoreEntityMention;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.HashMap;
 
 public class Model {
-    private Set<Entity> entities = new HashSet<Entity>();
-    private Set<Location> locations = new HashSet<Location>();
-    private Set<Relationship> relationships = new HashSet<Relationship>();
-    private Set<Scene> scenes = new HashSet<Scene>();
+    private Map<String, Entity> entities = new HashMap<String, Entity>();
+    private Map<String, Location> locations = new HashMap<String, Location>();
+    private List<Relationship> relationships = new ArrayList<Relationship>();
+    private List<Scene> scenes = new ArrayList<Scene>();
+    private Map<String, String> aliasMap = new HashMap();
 
 
     public Entity addEntity(CoreEntityMention em) {
         Entity newEntity = new Entity(em.text());
-        this.entities.add(newEntity);
+        this.entities.put(newEntity.getName(), newEntity);
         return newEntity;
     }
 
     public void addAlias(String entityName, String alias) {
-        for (Entity entity : this.entities) {
+        for (Entity entity : this.entities.values()) {
             if(entity.getName() == entityName) {
                 entity.addAlias(alias);
+                aliasMap.put(alias, entityName);
             }
+        }
+    }
+
+    public Entity getEntity(String name){
+        if (this.entities.containsKey(name)){
+            return this.entities.get(name);
+        }else if(this.aliasMap.containsKey(name)){
+            return this.entities.get(this.aliasMap.get(name));
+        }else {
+            return null;
         }
     }
 }
