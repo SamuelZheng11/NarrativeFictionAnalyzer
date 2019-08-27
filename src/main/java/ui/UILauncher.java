c701a10ce56a8fa271e2316bb0f6e0bccb3a07dd
samuelzheng11 <samuelzheng11@gmail.com>
2019-08-27 23:49:47 +1200

    move package structures around

--- src/main/java/ui/UILauncher.java
+++ src/main/java/ui/UILauncher.java
@@ -1,17 +1,10 @@
 package ui;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import launcher.Launcher;
-
-import java.awt.event.ActionEvent;
-import java.io.IOException;
 
 public class UILauncher extends Application {
 
